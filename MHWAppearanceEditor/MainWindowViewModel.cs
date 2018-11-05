66e94c56afb881406e565bc13a5bf3f1c7abc5f1
Fusion86 <fusion86@cerbus.nl>
2018-11-05 23:30:08 +0100

    Add basic GUI layout

---
+++ MHWAppearanceEditor/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Cirilla.Core.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace MHWAppearanceEditor
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SaveData SaveData { get; set; }
+        public List<SaveSlot> SaveSlots => SaveData?.SaveSlots;
+        public SaveSlot SelectedSaveSlot { get; set; }
+
+        public string ExportText { get; set; }
+
+        #region Commands
+
+        public RelayCommand OpenFileCommand { get; }
+        public RelayCommand SaveFileCommand { get; }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            OpenFileCommand = new RelayCommand(OpenFile, CanOpenFile);
+            SaveFileCommand = new RelayCommand(SaveFile, CanSaveFile);
+        }
+
+        private bool CanOpenFile() => true;
+        private void OpenFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == true)
+            {
+                try
+                {
+                    SaveData = new SaveData(ofd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error while opening file");
+                }
+            }
+        }
+
+        private bool CanSaveFile() => true;
+        private void SaveFile()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MHWAppearanceEditor
+
+Monster Hunter World - Appearance Import/Export
+
+[Cirilla.Core Source](https://github.com/Fusion86/Cirilla/tree/master/Cirilla.Core)
