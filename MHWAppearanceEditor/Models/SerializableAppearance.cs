0e0bb6fedf2fdbcc83c5f98d56b4c03ed0f66161
Fusion86 <fusion86@cerbus.nl>
2018-11-09 23:06:38 +0100

    Make values nullable
    This means that it won't reset fields that are undefined (in the import JSON) to null, aka zero. (E.g removing the "Gender" key would always set the gender to male, since 0 = male and 1 = female)

--- MHWAppearanceEditor/Models/SerializableAppearance.cs
+++ MHWAppearanceEditor/Models/SerializableAppearance.cs
@@ -10,65 +10,65 @@ namespace MHWAppearanceEditor.Models
 {
     public class SerializableAppearance
     {
-        public Gender Gender { get; set; }
+        public Gender? Gender { get; set; }
 
         #region Types
 
-        public byte EyeWidth { get; set; }
-        public byte EyeHeight { get; set; }
-        public byte SkinColorX { get; set; }
-        public byte SkinColorY { get; set; }
-        public byte Age { get; set; }
-        public byte NoseHeight { get; set; }
-        public byte MouthHeight { get; set; }
+        public byte? EyeWidth { get; set; }
+        public byte? EyeHeight { get; set; }
+        public byte? SkinColorX { get; set; }
+        public byte? SkinColorY { get; set; }
+        public byte? Age { get; set; }
+        public byte? NoseHeight { get; set; }
+        public byte? MouthHeight { get; set; }
 
-        public byte BrowType { get; set; }
-        public byte FaceType { get; set; }
-        public byte EyeType { get; set; }
-        public byte NoseType { get; set; }
-        public byte MouthType { get; set; }
-        public byte EyebrowType { get; set; }
-        public EyelashLength EyelashLength { get; set; }
-        public byte FacialHairType { get; set; }
+        public byte? BrowType { get; set; }
+        public byte? FaceType { get; set; }
+        public byte? EyeType { get; set; }
+        public byte? NoseType { get; set; }
+        public byte? MouthType { get; set; }
+        public byte? EyebrowType { get; set; }
+        public EyelashLength? EyelashLength { get; set; }
+        public byte? FacialHairType { get; set; }
 
-        public short HairType { get; set; }
-        public byte ClothingType { get; set; }
-        public byte Voice { get; set; }
-        public int Expression { get; set; }
+        public short? HairType { get; set; }
+        public byte? ClothingType { get; set; }
+        public byte? Voice { get; set; }
+        public int? Expression { get; set; }
 
         #endregion
 
         #region Colors
 
-        public Color LeftEyeColor { get; set; }
-        public Color RightEyeColor { get; set; }
-        public Color EyebrowColor { get; set; }
-        public Color FacialHairColor { get; set; }
+        public Color? LeftEyeColor { get; set; }
+        public Color? RightEyeColor { get; set; }
+        public Color? EyebrowColor { get; set; }
+        public Color? FacialHairColor { get; set; }
 
-        public Color HairColor { get; set; }
-        public Color ClothingColor { get; set; }
+        public Color? HairColor { get; set; }
+        public Color? ClothingColor { get; set; }
 
         #endregion
 
         #region Makeup
 
-        public Color Makeup1Color { get; set; }
-        public float Makeup1PosX { get; set; }
-        public float Makeup1PosY { get; set; }
-        public float Makeup1SizeX { get; set; }
-        public float Makeup1SizeY { get; set; }
-        public float Makeup1Glossy { get; set; }
-        public float Makeup1Metallic { get; set; }
-        public int Makeup1Type { get; set; }
+        public Color? Makeup1Color { get; set; }
+        public float? Makeup1PosX { get; set; }
+        public float? Makeup1PosY { get; set; }
+        public float? Makeup1SizeX { get; set; }
+        public float? Makeup1SizeY { get; set; }
+        public float? Makeup1Glossy { get; set; }
+        public float? Makeup1Metallic { get; set; }
+        public int? Makeup1Type { get; set; }
 
-        public Color Makeup2Color { get; set; }
-        public float Makeup2PosX { get; set; }
-        public float Makeup2PosY { get; set; }
-        public float Makeup2SizeX { get; set; }
-        public float Makeup2SizeY { get; set; }
-        public float Makeup2Glossy { get; set; }
-        public float Makeup2Metallic { get; set; }
-        public int Makeup2Type { get; set; }
+        public Color? Makeup2Color { get; set; }
+        public float? Makeup2PosX { get; set; }
+        public float? Makeup2PosY { get; set; }
+        public float? Makeup2SizeX { get; set; }
+        public float? Makeup2SizeY { get; set; }
+        public float? Makeup2Glossy { get; set; }
+        public float? Makeup2Metallic { get; set; }
+        public int? Makeup2Type { get; set; }
 
         #endregion
 
@@ -110,8 +110,11 @@ namespace MHWAppearanceEditor.Models
                 // And if it has the same property then set __saveSlot__ property to the same value
                 if (saveSlotProp != null)
                 {
-                    // This is the only thing that is different between this method and the constructor for this class, the destination where to __set__ the value
-                    saveSlotProp.SetValue(saveSlot, prop.GetValue(this));
+                    var value = prop.GetValue(this);
+
+                    // Don't set if null (since all fields are optional, and we dont want to set the Gender to Male (zero) when it is not included in the JSON)
+                    if (value != null)
+                        saveSlotProp.SetValue(saveSlot, value);
                 }
             }
         }
